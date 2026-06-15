62872b2cceecafd4c3dd67433f091656927be5ae
Cypria <290057852+SandrasCode@users.noreply.github.com>
2026-06-15 16:57:51 +0200

    Pyright says make it right

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas as pd # type: ignore
 students = pd.Series(['Al', 'Bo', 'Ce'], name = 'students')
 print("Lalala this one tries the CI pardy")
-students
+print(students)
